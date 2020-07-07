49aa75475b915ca7e466312f0170ef40e8e392dd
Hannah Riley <riley.hannahm@gmail.com>
2020-07-06 21:27:23 -0700

    basic project model implemented

---
+++ projectData/projects.js
@@ -0,0 +1,10 @@
+export const projects = [
+  {
+    projectTitle: 'Alchimia',
+    projectImage: '../src/assets/alchimiaImage.png',
+    projectUrl: 'https://hannahriley33.github.io/alchimia/',
+    projectGit: 'https://github.com/hannahriley33/alchimia',
+    projectStack: ['Javascript', 'HTML', 'CSS'],
+    summary: 'summary here'
+  }
+];
