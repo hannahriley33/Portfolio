c3b12b9a2f150171dfb61c27ea4395a80712979d
Hannah Riley <riley.hannahm@gmail.com>
2020-07-07 19:51:45 -0700

    added resume photo to resume component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -15,6 +15,7 @@ export default function App() {
         <Header />
         <Switch>
           <Route path="/resume" component={Resume} />
+          {/* <img src={} /> */}
         </Switch>
       </Router>
     </>
--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import resume from '../../assets/resume.png';
 
 const Resume = () => {
   
   return (
     <div>
       <p>RESUME</p>
+      <img src={resume}></img>
     </div>
   );
 };
