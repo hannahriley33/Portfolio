e5ad975934496c7222065e7ab81b42e91b5317a5
Hannah Riley <riley.hannahm@gmail.com>
2020-07-12 11:40:23 -0700

    updates

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -8,6 +8,7 @@ const Landing = () => {
     <div className={styles.mainDiv}>
       <img className={styles.mainPic} src={landingBeanie}></img>
       <div className={styles.welcomeText}>
+        <h1 className={styles.title}>{'Hi there, I\'m Hannah'}</h1>
         <p>
           {'Full-stack developer with a particular passion for solving human-facing problems. I thrive in environments where collaboration is key to building elegant and clean projects. I strongly value companies that support career growth and professional development. Previous positions have allowed me to hone in on my communication, creativity, and adaptability skills. Transitioning into web development has taught me that I can learn anything that I put a strong effort into. I am excited to tackle new challenges and learn new technologies.'}
         </p>
