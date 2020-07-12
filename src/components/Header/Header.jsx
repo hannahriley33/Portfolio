b1b3bb8258ead43ece42abc1385eaa1a58fced4d
Hannah Riley <riley.hannahm@gmail.com>
2020-07-12 09:35:17 -0700

    updated header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,26 @@ import styles from './headerStyles.css';
 
 const Header = () => {
 
+  
   return (
-    <section className={styles.parent}>
-      <div className={styles.git}>
-        <Link to="/projects"> <p className={styles.gitLink}>Projects</p> </Link>
+    <div className={styles.parent}>
+    
+      <div className={styles.home}>
+        <Link to="/"><p className={styles.link}>Home</p></Link>
+      </div>
+      <div className={styles.projectGit}>
+        <Link to="/projects"> <p className={styles.link}>Projects</p> </Link>
       </div>
       <div className={styles.site}>
-        <Link to="/resume"><p className={styles.siteLink}>Resume</p></Link>
+        <Link to="/resume"><p className={styles.link}>Resume</p></Link>
       </div>
-    </section>
+      <div className={styles.githubLink}>
+        <a href="https://github.com/hannahriley33" target="blank"><p className={styles.link}>Github</p></a>
+      </div>
+      <div className={styles.linkedinLink}>
+        <a href="https://www.linkedin.com/in/hannah-m-riley/" target="blank"><p className={styles.link}>Linkedin</p></a>
+      </div>
+    </div>
   );
 };
 
